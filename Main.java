1362da26cf4b3652351d9629f93c4c0e2b55e106
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-07-12 15:54:05 -0700

    Adding ability to guess more than one word. In progress

--- Main.java
+++ Main.java
@@ -9,14 +9,16 @@ class game_Attributes{
     int player_Tries = 3;
     // Hey i added the new feature
 
-    String[] words = {"hello","moon","swimming","basketball","lawn","venus","sunny","physics","building","city","country"
+    String[] wordsEasy = {"hello","moon","swimming","basketball","lawn","venus","sunny","physics","building","city","country"
             ,"africa","asia","earth","plant","road","train","biology","chicken","turkey","desert","antarctica","ocean","river","animal"};
+
+    String[] wordMedium = {"construction", "latitude"};
 
     String[] words_Meaning = {"greetings","space","activity","game","maintenance","space","felt","learning","shelter","landmass","landmass"
     ,"landmass","landmass","space","grows","built","built","learning","mmm hunger","mmm hunger","hot","cold","deep","natural","lives"};
 
-    int real_Random_Number = random_number.nextInt(words.length-1);
-    String Real_word = words[real_Random_Number];;
+    int real_Random_Number = random_number.nextInt(wordsEasy.length-1);
+    String Real_word = wordsEasy[real_Random_Number];
     boolean flag = true;
 }
 
@@ -59,6 +61,10 @@ class game_logic extends assigned_Values{
         System.out.println("JUMBLED WORD: " + swapped_String);
 
         while(flag){
+
+
+//            System.out.println(Real_word);
+//            System.out.println(swapped_String);
             player_Tries--;
             System.out.println("\nGUESS: ");
             player_Guess = scanner_One.next();
@@ -75,10 +81,14 @@ class game_logic extends assigned_Values{
                 System.out.println("--------------------------------------");
                 System.out.println("YOU GUESSED RIGHT! THE WORD WAS '" + Real_word + "'" );
                 System.out.println("--------------------------------------");
-                break;
+//                break;
             } else {
                 System.out.println("WRONG GUESS! GUESS AGAIN ...");
             }
+            real_Random_Number = random_number.nextInt(wordsEasy.length-1);
+            Real_word = wordsEasy[real_Random_Number];
+            jumbled_Word();
+            game_Start();
         }
     }
 
