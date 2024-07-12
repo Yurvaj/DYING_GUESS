1b8a80d650d0b731d97dffb8b4223540095ad8ac
Yuvraj Singh Rathore <yuvraj12341234@gmail.com>
2024-07-13 01:11:10 +0530

    Guess the 13/07/24

---
+++ Main.java
@@ -0,0 +1,82 @@
+package com.company;
+import java.util.*;
+
+class game_Attributes{
+    Random random_number = new Random();
+    Scanner scanner_One = new Scanner(System.in);
+    String player_Guess;
+
+    String[] words = {"hello","moon","swimming","basketball","lawn","venus","sunny","physics","building","city","country"
+            ,"africa","asia","earth","plant","road","train","biology","chicken","turkey","desert","antarctica","ocean","river","animal"};
+
+    int real_Random_Number = random_number.nextInt(words.length-1);
+    String Real_word = words[real_Random_Number];;
+    boolean flag = true;
+}
+
+class assigned_Values extends game_Attributes{
+    char[] character_Of_Jumbled_Word = Real_word.toCharArray();
+    String swapped_String;
+}
+
+
+
+class game_logic extends assigned_Values{
+
+    public void jumbled_Word(){
+        for (int i=0;i<Real_word.length();i++) {
+            if (i == 1){
+                char temp = character_Of_Jumbled_Word[0];
+                character_Of_Jumbled_Word[0] = character_Of_Jumbled_Word[1];
+                character_Of_Jumbled_Word[1] = temp;
+            } if (i == 3) {
+                char temp = character_Of_Jumbled_Word[2];
+                character_Of_Jumbled_Word[2] = character_Of_Jumbled_Word[3];
+                character_Of_Jumbled_Word[3] = temp;
+            }
+            if (i == 5) {
+                char temp = character_Of_Jumbled_Word[4];
+                character_Of_Jumbled_Word[4] = character_Of_Jumbled_Word[5];
+                character_Of_Jumbled_Word[5] = temp;
+            }
+            if (i == 7) {
+                char temp = character_Of_Jumbled_Word[6];
+                character_Of_Jumbled_Word[6] = character_Of_Jumbled_Word[7];
+                character_Of_Jumbled_Word[7] = temp;
+            }
+        }
+        swapped_String = new String(character_Of_Jumbled_Word);
+    }
+
+    public void game_Start(){
+        System.out.println("A jumbled word will be displayed on your screen & you must guess the correct word in order to win !\n");
+        System.out.println("JUMBLED WORD: " + swapped_String);
+
+        while(flag){
+            System.out.println("\nGUESS: ");
+            player_Guess = scanner_One.next();
+
+            if (player_Guess.equalsIgnoreCase(Real_word)){
+                System.out.println("--------------------------------------");
+                System.out.println("YOU GUESSED RIGHT! THE WORDS WAS '" + Real_word + "'" );
+                System.out.println("--------------------------------------");
+                break;
+            } else {
+                System.out.println("WRONG GUESS! GUESS AGAIN ...");
+            }
+        }
+    }
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        game_Attributes GA = new game_Attributes();
+        game_logic GL = new game_logic();
+        GL.jumbled_Word();
+        GL.game_Start();
+
+
+    }
+}
